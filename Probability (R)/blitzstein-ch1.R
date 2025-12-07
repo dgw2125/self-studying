3e8aee095ef89e4b5085819cee927b52e8966b75
David Wang <wisedavidg@gmail.com>
2025-12-07 13:28:35 -0500

    Revised and polished blitzstein-ch1.R file

--- Probability (R)/blitzstein-ch1.R
+++ Probability (R)/blitzstein-ch1.R
@@ -1,4 +1,26 @@
-v <- c(3,1,4,1,5,9)
+# BASICS - of R
+v <- c(3,1,4,1,5,9) # assign a variable a vector value
+y <- 5 # assign a variable a value; this is equivalent to y <- c(5)
+
+sum(v) # returns the sum of the entries in vector v
+max(v) # returns the maximum value amongst the entries in vector v
+v[1] # returns the 1st entry; change the value in the brackets to get the nth entry
+v[c(1,3,5)] # returns the entries in the positions specified in the vector contained within the brackets
+v[-c(1,4)] # returns the entries in the vector v, excluding those in the specified positions
+z <- (2:8) # returns a vector with entries between 2 and 8 inclusive. This is generalizable for integers m and n.
+
+1/(1:100)^2 # returns a vector (1, 1/2^2, 1/3^2, ...). This shows how many operations in R are interpretted component wise
+
+factorial(10) # how to express factorials
+choose(10,2) # how to express combinations
+lfactorial(1000) # returns log of a factorial
+lchoose(1000,400) # returns log of a combination
+
+sample(10,5) # returns a "random" (simulates random, but is actually deterministic) sample of 5 elements from the vector 1:10
+sample(10,5,replace=TRUE) # takes a random sample WITH replacement
+sample(letters,7) # R has a built in vector letters consistenign of the 26 lowercase letters
+sample(4,3,replace=TRUE,prob=c(0.1,0.2,0.3,0.4)) # you can specify general probabilities for sampling each element
+
 
 
 # SIMULATION - de Montmort's Matching Problem
@@ -13,6 +35,7 @@ r <- replicate(10^4, sum(sample(n)==(1:n)))
 
 sum(r>=1)/10^4
 # sum(r>=1) returns the sum of times r>=1 is TRUE for each parameter of the vector
+
 
 
 # SIMULATION - Matching Birthdays
@@ -37,6 +60,7 @@ sum(r>=2)/10^4
 # mathematical formula
 # calculates the probability there's a shared birthday by using the probability nobody shares a birthday
 # prod gives the product of a vector; in this case it looks like 365 * 364 * ... (365-k+1)
+
 
 
 # BUILT IN FORMULAS - Matching Birthdays
